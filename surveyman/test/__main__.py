e8b440e434b6c25be68fd7cf29f7a294a4a74c74
etosch <etosch@gmail.com>
2014-08-25 14:39:03 -0400

    now spitting out json with the tests

--- surveyman/test/__main__.py
+++ surveyman/test/__main__.py
@@ -3,6 +3,11 @@ import unittest
 import BlockTests
 import ConstraintTests
 import SurveyTests
+import surveyman.examples.SimpleSurvey as simple
+import surveyman.examples.example_survey as example
+import surveyman.examples.subblock_example as sub
+import surveyman.jsonValidator as validator
+import json
 
 unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(BlockTests.BlockTests))
 unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(ConstraintTests.ConstraintTests))
@@ -12,4 +17,14 @@ unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTest
 unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(SurveyTests.ConstraintTests))
 unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(SurveyTests.SurveyTests))
 
+# dump surveys to json
+ex1 = simple.create_survey()
+ex2 = example.create_survey()
+ex3 = sub.create_survey()
+examples = [ex1, ex2, ex3]
 
+for (i, ex) in enumerate(examples):
+    json_survey = ex.jsonize()
+    validator.validate_json(json.loads(json_survey))
+    with open("ex"+str(i)+".json", "w") as f:
+        f.write(json_survey)
