9fd511ad84f75725c7e90ea89550cec68b3555a7
etosch <etosch@gmail.com>
2015-03-18 17:27:02 -0400

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='SMPy',
-    version='1.0.2',
+    version='1.0.3',
     author='Emma Tosch',
     author_email='etosch@cs.umass.edu',
     packages=find_packages(),
